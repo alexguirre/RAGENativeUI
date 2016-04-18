352843234379a598b44245e49ea30118d0066b54
alexguirre <alexguirre.aa@gmail.com>
2016-04-18 18:08:42 +0200

    Create UIMenuSwitchMenusItem.cs

---
+++ Source/Elements/UIMenuSwitchMenusItem.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RAGENativeUI.Elements
+{
+    /// <summary>
+    /// List item that holds an <see cref="UIMenu"/> list and allows to easily switch between those menus.
+    /// </summary>
+    /// <remarks>
+    /// All the <see cref="UIMenu"/>s must have the same instance of this <see cref="UIMenuSwitchMenusItem"/> and
+    /// is recommended to use the property <see cref="UIMenuSwitchMenusItem.CurrentMenu"/> to make the menu visible without issues.
+    /// </remarks>
+    public class UIMenuSwitchMenusItem : UIMenuListItem
+    {
+        private List<UIMenu> _menus;
+
+        private UIMenu _currentMenu;
+        /// <summary>
+        /// The currently selected menu. Use this to make the proper menu visible when using this item.
+        /// </summary>
+        public UIMenu CurrentMenu
+        {
+            get
+            {
+                return _currentMenu;
+            }
+            set
+            {
+                if (!_menus.Contains(value))
+                    throw new ArgumentException("The item doesn't contain the specified UIMenu", "value");
+                if (!value.MenuItems.Contains(this))
+                    throw new ArgumentException("The specified UIMenu doesn't contain this item", "value");
+                _currentMenu = value;
+                _currentMenu.CurrentSelection = _currentMenu.MenuItems.IndexOf(this);
+                Index = _menus.IndexOf(_currentMenu);
+            }
+        }
+
+        /// <summary>
+        /// List item, with left/right arrows that switches the current menu depending on the current <see cref="UIMenu"/> item.
+        /// Uses the menus titles as the names in the list.
+        /// </summary>
+        /// <param name="text">Item label.</param>
+        /// <param name="menus">List that contains your <see cref="UIMenu"/>s.</param>
+        /// <param name="index">Index in the list. If unsure user 0.</param>
+        public UIMenuSwitchMenusItem(string text, List<UIMenu> menus, int index)
+            : this(text, menus, new List<string>(menus.Select(m => m.Title.Caption)), index, "")
+        {
+        }
+
+        /// <summary>
+        /// List item, with left/right arrows that switches the current menu depending on the current <see cref="UIMenu"/> item.
+        /// Uses the menus titles as the names in the list.
+        /// </summary>
+        /// <param name="text">Item label.</param>
+        /// <param name="menus">List that contains your <see cref="UIMenu"/>s.</param>
+        /// <param name="index">Index in the list. If unsure user 0.</param>
+        /// <param name="description">Description for this item.</param>
+        public UIMenuSwitchMenusItem(string text, List<UIMenu> menus, int index, string description) 
+            : this(text, menus, new List<string>(menus.Select(m => m.Title.Caption)), index, description)
+        {
+        }
+
+        /// <summary>
+        /// List item, with left/right arrows that switches the current menu depending on the current <see cref="UIMenu"/> item.
+        /// The menus list and the menus names list must have the same items count.
+        /// </summary>
+        /// <param name="text">Item label.</param>
+        /// <param name="menus">List that contains your <see cref="UIMenu"/>s.</param>
+        /// <param name="menusNames">List that contains a name for each <see cref="UIMenu"/> in the same order</param>
+        /// <param name="index">Index in the list. If unsure user 0.</param>
+        public UIMenuSwitchMenusItem(string text, List<UIMenu> menus, List<string> menusNames, int index)
+            : this(text, menus, menusNames, index, "")
+        {
+        }
+
+        /// <summary>
+        /// List item, with left/right arrows that switches the current menu depending on the current <see cref="UIMenu"/> item.
+        /// The menus list and the menus names list must have the same items count.
+        /// </summary>
+        /// <param name="text">Item label.</param>
+        /// <param name="menus">List that contains your <see cref="UIMenu"/>s.</param>
+        /// <param name="menusNames">List that contains a name for each <see cref="UIMenu"/> in the same order</param>
+        /// <param name="index">Index in the list. If unsure user 0.</param>
+        /// <param name="description">Description for this item.</param>
+        public UIMenuSwitchMenusItem(string text, List<UIMenu> menus, List<string> menusNames, int index, string description)
+            : base(text, new List<dynamic>(menusNames), index, description)
+        {
+            if (menus.Count != menusNames.Count)
+                throw new ArgumentException("The UIMenu list and the names strings list must have the same items count", "menus, menusNames");
+
+            _menus = new List<UIMenu>(menus);
+            _currentMenu = IndexToMenu(index);
+        }
+
+
+        /// <summary>
+        /// Find a menu in the list and return it's index.
+        /// </summary>
+        /// <param name="menu">Menu to search for.</param>
+        /// <returns>Item index.</returns>
+        public virtual int MenuToIndex(UIMenu menu)
+        {
+            return _menus.FindIndex(m => m == menu);
+        }
+
+
+        /// <summary>
+        /// Find a menu by it's index and return the menu.
+        /// </summary>
+        /// <param name="index">Menu's index.</param>
+        /// <returns>Menu</returns>
+        public virtual UIMenu IndexToMenu(int index)
+        {
+            return _menus[index];
+        }
+
+
+        internal override void ListChangedTrigger(int newindex)
+        {
+            CurrentMenu.Visible = false;
+            CurrentMenu = _menus[newindex % _menus.Count];
+            CurrentMenu.Visible = true;
+            base.ListChangedTrigger(newindex);
+        }
+    }
+}
+
