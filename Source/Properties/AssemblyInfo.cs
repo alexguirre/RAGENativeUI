ea63d1625d02c6dc514474bdf56230dd01effad2
alexguirre <alexguirre.aa@gmail.com>
2016-09-06 18:00:55 +0200

    Update AssemblyVersion to 1.5.0

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f3e16ed9-dbf7-4e7b-b04b-9b24b11891d3")]
-[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 
