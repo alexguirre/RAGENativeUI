d1522204e22bd37bba4d5a3219dce84df7f18c04
alexguirre <alexguirre.aa@gmail.com>
2017-03-19 23:17:54 +0100

    Make InstructionalButton ButtonText and ButtonControl public
    And added documentation

--- Source/Elements/InstructionalButtons.cs
+++ Source/Elements/InstructionalButtons.cs
@@ -39,7 +39,7 @@ namespace RAGENativeUI.Elements
                 InstructionalButton b = Buttons[i];
                 if (b.CanBeDisplayed == null || b.CanBeDisplayed.Invoke(b))
                 {
-                    Scaleform.CallFunction("SET_DATA_SLOT", dateSlotIndex++, b.GetButtonId(), b.Text);
+                    Scaleform.CallFunction("SET_DATA_SLOT", dateSlotIndex++, b.GetButtonId(), b.Text ?? "");
                 }
             }
             Scaleform.CallFunction("DRAW_INSTRUCTIONAL_BUTTONS", -1);
@@ -53,40 +53,82 @@ namespace RAGENativeUI.Elements
 
     public class InstructionalButton
     {
+        /// <summary>
+        /// Gets or sets the text displayed next to the button.
+        /// <para>
+        /// If this <see cref="InstructionalButton"/> is contained in a <see cref="InstructionalButtons"/> scaleform, 
+        /// <see cref="InstructionalButtons.Update"/> needs to be called to reflect the changes made.
+        /// </para>
+        /// </summary>
+        /// <value>
+        /// A <see cref="String"/> representing the text displayed next to the button.
+        /// </value>
         public string Text { get; set; }
 
+        /// <summary>
+        /// Gets or sets the text displayed inside the button, mainly for displaying custom keyboard bindings, like "I", or "O", or "F5".
+        /// <para>
+        /// If <c>null</c>, <see cref="ButtonControl"/> is used instead.
+        /// </para>
+        /// <para>
+        /// If this <see cref="InstructionalButton"/> is contained in a <see cref="InstructionalButtons"/> scaleform, 
+        /// <see cref="InstructionalButtons.Update"/> needs to be called to reflect the changes made.
+        /// </para>
+        /// </summary>
+        /// <value>
+        /// A <see cref="String"/> representing the text displayed inside the button.
+        /// </value>
+        public string ButtonText { get; set; }
+        /// <summary>
+        /// Gets or sets the <see cref="GameControl"/> displayed inside the button, it changes depending on keybinds and whether the user is using the controller or the keyboard and mouse.
+        /// <para>
+        /// If <c>null</c> and <see cref="ButtonText"/> is also <c>null</c> an empty button is displayed.
+        /// </para>
+        /// <para>
+        /// If this <see cref="InstructionalButton"/> is contained in a <see cref="InstructionalButtons"/> scaleform, 
+        /// <see cref="InstructionalButtons.Update"/> needs to be called to reflect the changes made.
+        /// </para>
+        /// </summary>
+        /// <value>
+        /// A <see cref="GameControl"/> representing the binding displayed inside the button.
+        /// </value>
+        public GameControl? ButtonControl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Predicate{InstructionalButton}"/> delegate that defines the conditions for this <see cref="InstructionalButton"/> to be visible.
+        /// <para>
+        /// If this <see cref="InstructionalButton"/> is contained in a <see cref="InstructionalButtons"/> scaleform, 
+        /// this predicate is only evaluated when <see cref="InstructionalButtons.Update"/> is called.
+        /// </para>
+        /// </summary>
+        /// <value>
+        /// A <see cref="Predicate{InstructionalButton}"/> delegate that defines the conditions for this <see cref="InstructionalButton"/> to be visible.
+        /// </value>
         public Predicate<InstructionalButton> CanBeDisplayed { get; set; }
 
         [Obsolete("ItemBind is obsolete. Check BindToItem(UIMenuItem) for details.")]
         public UIMenuItem ItemBind { get; private set; }
 
-        private readonly string buttonString;
-        private readonly GameControl buttonControl;
-        private readonly bool usingControls;
-
         /// <summary>
-        /// Add a dynamic button to the instructional buttons array.
-        /// Changes whether the controller is being used and changes depending on keybinds.
+        /// Initializes a new instance of the <see cref="InstructionalButton"/> class.
         /// </summary>
-        /// <param name="control">Rage.GameControl that gets converted into a button.</param>
-        /// <param name="text">Help text that goes with the button.</param>
+        /// <param name="control">The <see cref="GameControl"/> displayed inside the button, it changes depending on keybinds and whether the user is using the controller or the keyboard and mouse.</param>
+        /// <param name="text">The text displayed next to the button.</param>
         public InstructionalButton(GameControl control, string text)
         {
             Text = text;
-            buttonControl = control;
-            usingControls = true;
+            ButtonControl = control;
         }
 
         /// <summary>
-        /// Adds a keyboard button to the instructional buttons array.
+        /// Initializes a new instance of the <see cref="InstructionalButton"/> class.
         /// </summary>
-        /// <param name="keystring">Custom keyboard button, like "I", or "O", or "F5".</param>
-        /// <param name="text">Help text that goes with the button.</param>
-        public InstructionalButton(string keystring, string text)
+        /// <param name="buttonText">The text displayed inside the button, mainly for displaying custom keyboard bindings, like "I", or "O", or "F5".</param>
+        /// <param name="text">The text displayed next to the button.</param>
+        public InstructionalButton(string buttonText, string text)
         {
             Text = text;
-            buttonString = keystring;
-            usingControls = false;
+            ButtonText = buttonText;
         }
 
         /// <summary>
@@ -99,10 +141,10 @@ namespace RAGENativeUI.Elements
             ItemBind = item;
             CanBeDisplayed = (i) => item.Selected;
         }
-
+        
         public string GetButtonId()
         {
-            return usingControls ? GetButtonId(buttonControl) : GetButtonId(buttonString);
+            return ButtonText == null ? (ButtonControl.HasValue ? GetButtonId(ButtonControl.Value) : GetButtonId("")) : GetButtonId(ButtonText);
         }
 
         public static string GetButtonId(GameControl control)
