1827a011a9c5b01c3cb1f8e4a4f2549df72074e0
alexguirre <alexguirre.aa@gmail.com>
2017-04-29 03:45:55 +0200

    Add UIMenuItem color properties
    BackColor, HighlightedBackColor, ForeColor and HighlightedForeColor.
    And marked UIMenuColoredItem as obsolete.

--- Source/Elements/UIMenuColoredItem.cs
+++ Source/Elements/UIMenuColoredItem.cs
@@ -1,14 +1,20 @@
-﻿using System.Drawing;
-
-namespace RAGENativeUI.Elements
+﻿namespace RAGENativeUI.Elements
 {
+    using System;
+    using System.Drawing;
+
+    [Obsolete("UIMenuColoredItem is obsolete, use the UIMenuItem's color properties instead.")]
     public class UIMenuColoredItem : UIMenuItem
     {
-        public Color MainColor { get; set; }
-        public Color HighlightColor { get; set; }
+        [Obsolete("UIMenuColoredItem is obsolete, use the UIMenuItem.BackColor property instead.")]
+        public Color MainColor { get { return BackColor; } set { BackColor = value; } }
+        [Obsolete("UIMenuColoredItem is obsolete, use the UIMenuItem.BackColor property instead.")]
+        public Color HighlightColor { get { return HighlightedBackColor; } set { HighlightedBackColor = value; } }
 
-        public Color TextColor { get; set; }
-        public Color HighlightedTextColor { get; set; }
+        [Obsolete("UIMenuColoredItem is obsolete, use the UIMenuItem.TextColor property instead.")]
+        public Color TextColor { get { return ForeColor; } set { ForeColor = value; } }
+        [Obsolete("UIMenuColoredItem is obsolete, use the UIMenuItem.HighlightedTextColor property instead.")]
+        public Color HighlightedTextColor { get { return HighlightedForeColor; } set { HighlightedForeColor = value; } }
 
         public UIMenuColoredItem(string label, Color color, Color highlightColor) : base(label)
         {
@@ -17,8 +23,6 @@ namespace RAGENativeUI.Elements
 
             TextColor = Color.White;
             HighlightedTextColor = Color.Black;
-
-            Init();
         }
 
         public UIMenuColoredItem(string label, string description, Color color, Color highlightColor) : base(label, description)
@@ -28,77 +32,10 @@ namespace RAGENativeUI.Elements
 
             TextColor = Color.White;
             HighlightedTextColor = Color.Black;
-
-            Init();
         }
 
         protected void Init()
         {
-            _selectedSprite = new Sprite("commonmenu", "gradient_nav", new Point(0, 0), new Size(431, 38), 0, HighlightColor);
-            _rectangle = new ResRectangle(new Point(0, 0), new Size(431, 38), Color.FromArgb(150, 0, 0, 0));
-            _text = new ResText(Text, new Point(8, 0), 0.33f, Color.WhiteSmoke, Common.EFont.ChaletLondon, ResText.Alignment.Left);
-            Description = Description;
-
-            _badgeLeft = new Sprite("commonmenu", "", new Point(0, 0), new Size(40, 40));
-            _badgeRight = new Sprite("commonmenu", "", new Point(0, 0), new Size(40, 40));
-
-            _labelText = new ResText("", new Point(0, 0), 0.35f) { TextAlignment = ResText.Alignment.Right };
-        }
-
-
-        public override void Draw()
-        {
-            _rectangle.Size = new Size(431 + Parent.WidthOffset, 38);
-            _selectedSprite.Size = new Size(431 + Parent.WidthOffset, 38);
-
-            if (Hovered && !Selected)
-            {
-                _rectangle.Color = Color.FromArgb(20, 255, 255, 255);
-                _rectangle.Draw();
-            }
-            if (Selected)
-            {
-                _selectedSprite.Color = HighlightColor;
-                _selectedSprite.Draw();
-            }
-            else
-            {
-                _selectedSprite.Color = MainColor;
-                _selectedSprite.Draw();
-            }
-
-            _text.Color = Enabled ? Selected ?  HighlightedTextColor : TextColor : Color.FromArgb(163, 159, 148);
-
-            if (LeftBadge != BadgeStyle.None)
-            {
-                _text.Position = new Point(35 + Offset.X, _text.Position.Y);
-                _badgeLeft.TextureDictionary = BadgeToSpriteLib(LeftBadge);
-                _badgeLeft.TextureName = BadgeToSpriteName(LeftBadge, Selected);
-                _badgeLeft.Color = BadgeToColor(LeftBadge, Selected);
-                _badgeLeft.Draw();
-            }
-            else
-            {
-                _text.Position = new Point(8 + Offset.X, _text.Position.Y);
-            }
-
-            if (RightBadge != BadgeStyle.None)
-            {
-                _badgeRight.Position = new Point(385 + Offset.X + Parent.WidthOffset, _badgeRight.Position.Y);
-                _badgeRight.TextureDictionary = BadgeToSpriteLib(RightBadge);
-                _badgeRight.TextureName = BadgeToSpriteName(RightBadge, Selected);
-                _badgeRight.Color = BadgeToColor(RightBadge, Selected);
-                _badgeRight.Draw();
-            }
-
-            if (!string.IsNullOrWhiteSpace(RightLabel))
-            {
-                _labelText.Position = new Point(420 + Offset.X + Parent.WidthOffset, _labelText.Position.Y);
-                _labelText.Caption = RightLabel;
-                _labelText.Color = _text.Color = Enabled ? Selected ? HighlightedTextColor : TextColor : Color.FromArgb(163, 159, 148);
-                _labelText.Draw();
-            }
-            _text.Draw();
         }
     }
 }
