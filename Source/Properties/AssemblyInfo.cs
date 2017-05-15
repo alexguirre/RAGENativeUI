8677a535478358d7e4ef5b960823cde7f4ad6d1d
alexguirre <alexguirre.aa@gmail.com>
2017-05-15 19:10:05 +0200

    Clean up

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-
 [assembly: AssemblyTitle("RAGENativeUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RAGENativeUI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("f3e16ed9-dbf7-4e7b-b04b-9b24b11891d3")]
-[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: Guid("fc3d6fad-db94-4c47-9b48-3b783398bdc5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
