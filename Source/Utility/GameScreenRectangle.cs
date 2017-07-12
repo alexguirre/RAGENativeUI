68ac33ab20bf2c487b58a152305aa80b2a662eb8
alexguirre <alexguirre.aa@gmail.com>
2017-07-13 01:36:10 +0200

    Add GameScreenPosition

--- Source/Utility/GameScreenRectangle.cs
+++ Source/Utility/GameScreenRectangle.cs
@@ -16,10 +16,10 @@ namespace RAGENativeUI.Utility
         {
             if (absolute)
             {
-                int screenw = Game.Resolution.Width;
-                int screenh = Game.Resolution.Height;
+                int screenWidth = Game.Resolution.Width;
+                int screenHeight = Game.Resolution.Height;
                 const float height = 1080f;
-                float ratio = (float)screenw / screenh;
+                float ratio = (float)screenWidth / screenHeight;
                 var width = height * ratio;
 
                 float w = coords.Width / width;
@@ -53,6 +53,46 @@ namespace RAGENativeUI.Utility
         public static GameScreenRectangle FromRelativeCoords(float x, float y, float width, float height) => FromRelativeCoords(new RectangleF(x, y, width, height));
         public static GameScreenRectangle FromRelativeCoords(PointF position, SizeF size) => FromRelativeCoords(new RectangleF(position.X, position.Y, size.Width, size.Height));
         public static GameScreenRectangle FromRelativeCoords(RectangleF rectangle) => new GameScreenRectangle(rectangle, false);
+    }
+
+    public struct GameScreenPosition
+    {
+        public float X { get; }
+        public float Y { get; }
+
+        private GameScreenPosition(PointF coords, bool absolute)
+        {
+            if (absolute)
+            {
+                int screenWidth = Game.Resolution.Width;
+                int screenHeight = Game.Resolution.Height;
+                const float height = 1080f;
+                float ratio = (float)screenWidth / screenHeight;
+                var width = height * ratio;
+                
+                float x = (coords.X / width);
+                float y = (coords.Y / height);
+
+                X = x;
+                Y = y;
+            }
+            else
+            {
+                X = coords.X;
+                Y = coords.Y;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X} Y: {Y}";
+        }
+
+        public static GameScreenPosition FromAbsoluteCoords(float x, float y) => FromAbsoluteCoords(new PointF(x, y));
+        public static GameScreenPosition FromAbsoluteCoords(PointF position) => new GameScreenPosition(position, true);
+
+        public static GameScreenPosition FromRelativeCoords(float x, float y) => FromRelativeCoords(new PointF(x, y));
+        public static GameScreenPosition FromRelativeCoords(PointF position) => new GameScreenPosition(position, false);
     }
 }
 
