a1cfb4f9a0d4bccb956e9113627dcd7868ce30a3
alexguirre <alexguirre.aa@gmail.com>
2017-07-18 10:32:14 +0200

    Add InstructionalButtons

---
+++ Examples/InstructionalButtonsExample.cs
@@ -0,0 +1,70 @@
+namespace Examples
+{
+    using System;
+    using System.Drawing;
+
+    using Rage;
+    using Rage.Native;
+    using Rage.Attributes;
+    using Graphics = Rage.Graphics;
+
+    using RAGENativeUI;
+    using RAGENativeUI.Elements;
+    using RAGENativeUI.Utility;
+
+    internal static class InstructionalButtonsExample
+    {
+        [ConsoleCommand(Name = "InstructionalButtonsExample", Description = "Example showing the BigMessage class.")]
+        private static void Command()
+        {
+            InstructionalButtons instructionalButtons = new InstructionalButtons();
+            instructionalButtons.AddSlot("RappelJump 0", 0, GameControl.RappelJump);
+            instructionalButtons.AddSlot("RappelJump 1", 1, GameControl.RappelJump);
+            instructionalButtons.AddSlot("RappelJump 2", 2, GameControl.RappelJump);
+            instructionalButtons.AddSlot("Label", "Text");
+            instructionalButtons.AddSlot("Key", "K", InstructionalButtonStyle.Key);
+            instructionalButtons.AddSlot("MultiCharKey", "KEY", InstructionalButtonStyle.MultiCharKey);
+            instructionalButtons.AddSlot("WideKey", "WIDE", InstructionalButtonStyle.WideKey);
+            instructionalButtons.AddSlot("Reload", GameControl.Reload);
+            instructionalButtons.AddSlot("MoveUpDown", GameControl.MoveUpDown);
+            instructionalButtons.AddSlot("MoveLeftRight", GameControl.MoveLeftRight);
+            foreach (GameControl control in Enum.GetValues(typeof(GameControl)))
+            {
+                if (control.ToString().StartsWith("Frontend"))
+                {
+                    instructionalButtons.AddSlot(control.ToString(), control);
+                }
+            }
+
+            GameFiber.StartNew(() =>
+            {
+                while (true)
+                {
+                    GameFiber.Yield();
+                    
+                    instructionalButtons.Draw();
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Y))
+                    {
+                        if (instructionalButtons.Layout == InstructionalButtons.LayoutType.Horizontal)
+                            instructionalButtons.Layout = InstructionalButtons.LayoutType.Vertical;
+                        else
+                            instructionalButtons.Layout = InstructionalButtons.LayoutType.Horizontal;
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.U))
+                    {
+                        instructionalButtons.BackgroundColor = Color.FromArgb(MathHelper.GetRandomInteger(0, 255), MathHelper.GetRandomInteger(0, 255), MathHelper.GetRandomInteger(0, 255), MathHelper.GetRandomInteger(0, 255));
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.I))
+                    {
+                        instructionalButtons.BackgroundColor = Color.FromArgb(80, 0, 0, 0);
+                    }
+
+                }
+            });
+        }
+    }
+}
+
---
+++ Source/Elements/InstructionalButtons.cs
@@ -0,0 +1,156 @@
+namespace RAGENativeUI.Elements
+{
+    using System;
+    using System.Drawing;
+
+    using Rage;
+    using Rage.Native;
+
+    using RAGENativeUI.Utility;
+
+    public class InstructionalButtons : Scaleform
+    {
+        private InstructionalButtonsSlots slots;
+        /// <exception cref="ArgumentNullException">When setting the property to a null value.</exception>
+        public InstructionalButtonsSlots Slots { get { return slots; } set { slots = value ?? throw new ArgumentNullException($"The instruction buttons {nameof(Slots)} can't be null."); } }
+        public LayoutType Layout { get; set; } = LayoutType.Horizontal;
+        public Color BackgroundColor { get; set; } = Color.FromArgb(80, 0, 0, 0);
+
+        public InstructionalButtons() : base("instructional_buttons")
+        {
+            Slots = new InstructionalButtonsSlots();
+        }
+        
+        private void Update()
+        {
+            CallMethod("SET_DATA_SLOT_EMPTY");
+
+            CallMethod("TOGGLE_MOUSE_BUTTONS", false);
+
+            for (int i = 0; i < Slots.Count; i++)
+            {
+                InstructionalButtonSlot s = Slots[i];
+                CallMethod("SET_DATA_SLOT", i, s.GetButtonId(), s.Label);
+            }
+
+            CallMethod("DRAW_INSTRUCTIONAL_BUTTONS", (int)Layout);
+
+            CallMethod("SET_BACKGROUND_COLOUR", BackgroundColor.R, BackgroundColor.G, BackgroundColor.B, BackgroundColor.A);
+        }
+
+        public override void Draw(Color color)
+        {
+            Update();
+            base.Draw(color);
+        }
+
+        public override void Draw(GameScreenRectangle rectangle, Color color)
+        {
+            Update();
+            base.Draw(rectangle, color);
+        }
+
+        public override void Draw3D(Vector3 position, Rotator rotation, Vector3 scale)
+        {
+            Update();
+            base.Draw3D(position, rotation, scale);
+        }
+
+        public int AddSlot(InstructionalButtonSlot slot)
+        {
+            int slotIndex = Slots.Count;
+            Slots.Add(slot);
+            return slotIndex;
+        }
+
+        public int AddSlot(string label, int index, GameControl control) => AddSlot(new InstructionalButtonControlSlot(label, index, control));
+        public int AddSlot(string label, GameControl control) => AddSlot(new InstructionalButtonControlSlot(label, control));
+        public int AddSlot(string label, string text, InstructionalButtonStyle style) => AddSlot(new InstructionalButtonTextSlot(label, text, style));
+        public int AddSlot(string label, string text) => AddSlot(new InstructionalButtonTextSlot(label, text));
+
+
+        public class InstructionalButtonsSlots : BaseCollection<InstructionalButtonSlot>
+        {
+        }
+
+        public enum LayoutType
+        {
+            Horizontal = -1,
+            Vertical = 1,
+        }
+    }
+
+    public abstract class InstructionalButtonSlot
+    {
+        public string Label { get; set; }
+
+        public InstructionalButtonSlot(string label)
+        {
+            Label = label;
+        }
+
+        public abstract string GetButtonId();
+    }
+
+    public class InstructionalButtonControlSlot : InstructionalButtonSlot
+    {
+        public int Index { get; set; }
+        public GameControl Control { get; set; }
+
+        public InstructionalButtonControlSlot(string label, int index, GameControl control) : base(label)
+        {
+            Index = index;
+            Control = control;
+        }
+
+        public InstructionalButtonControlSlot(string label, GameControl control) : this(label, 0, control)
+        {
+        }
+
+        public override string GetButtonId()
+        {
+            return NativeFunction.Natives.GetControlInstructionalButton<string>(Index, (int)Control, 0);
+        }
+    }
+
+    public class InstructionalButtonTextSlot : InstructionalButtonSlot
+    {
+        public string Text { get; set; }
+        public InstructionalButtonStyle Style { get; set; }
+
+        public InstructionalButtonTextSlot(string label, string text, InstructionalButtonStyle style) : base(label)
+        {
+            Text = text;
+            Style = style;
+        }
+
+        public InstructionalButtonTextSlot(string label, string text) : this(label, text, InstructionalButtonStyle.Key)
+        {
+        }
+
+        public override string GetButtonId()
+        {
+            return GetStyleButtonIdPrefix(Style) + Text;
+        }
+
+        public static string GetStyleButtonIdPrefix(InstructionalButtonStyle style)
+        {
+            switch (style)
+            {
+                case InstructionalButtonStyle.Key: return "t_";
+                case InstructionalButtonStyle.MultiCharKey: return "T_";
+                case InstructionalButtonStyle.WideKey: return "w_";
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(style));
+        }
+    }
+
+    public enum InstructionalButtonStyle
+    {
+        Key,
+        MultiCharKey,
+        WideKey,
+    }
+}
+
