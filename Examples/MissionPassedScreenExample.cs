d7bf5ed388140113a264db142883d3406b626161
alexguirre <alexguirre.aa@gmail.com>
2017-07-19 05:24:58 +0200

    Add MissionPassedScreen

---
+++ Examples/MissionPassedScreenExample.cs
@@ -0,0 +1,73 @@
+namespace Examples
+{
+    using System;
+    using System.Drawing;
+
+    using Rage;
+    using Rage.Native;
+    using Rage.Attributes;
+    using Graphics = Rage.Graphics;
+
+    using RAGENativeUI;
+    using RAGENativeUI.Elements;
+    using RAGENativeUI.Utility;
+
+    internal static class MissionPassedScreenExample
+    {
+        [ConsoleCommand(Name = "MissionPassedScreenExample", Description = "Example showing the MissionPassedScreen class.")]
+        private static void Command()
+        {
+            MissionPassedScreen missionPassedScreen = new MissionPassedScreen("mission passed", "Hotel Assassination");
+            missionPassedScreen.Items.Add(new MissionPassedScreenItem("Time Taken", "04:18"));
+            missionPassedScreen.Items.Add(new MissionPassedScreenItem("Sniper Kill Bonus", "", MissionPassedScreenItem.TickboxState.Tick));
+            missionPassedScreen.Items.Add(new MissionPassedScreenItem("Money Earned", "$9,000", MissionPassedScreenItem.TickboxState.Tick));
+            missionPassedScreen.IsCompletionVisible = true;
+            missionPassedScreen.Continued += (s) => Game.DisplayHelp("OnContinued");
+
+            int i = 0;
+            GameFiber.StartNew(() =>
+            {
+                while (true)
+                {
+                    GameFiber.Yield();
+
+                    Game.DisplayHelp($"IsVisible: {missionPassedScreen.IsVisible}");
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Y))
+                    {
+                        missionPassedScreen.Show();
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.H))
+                    {
+                        missionPassedScreen.Hide();
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.U))
+                    {
+                        missionPassedScreen.IsCompletionVisible = !missionPassedScreen.IsCompletionVisible;
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.J))
+                    {
+                        missionPassedScreen.ScreenEffect = MathHelper.Choose("SuccessFranklin", "SuccessTrevor", "SuccessMichael", null);
+                        Game.DisplayNotification(missionPassedScreen.ScreenEffect ?? "null");
+                    }
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Add))
+                    {
+                        missionPassedScreen.Items.Add(new MissionPassedScreenItem("Item #" + i++, MathHelper.Choose("Status", "", ""), MathHelper.Choose((MissionPassedScreenItem.TickboxState[])Enum.GetValues(typeof(MissionPassedScreenItem.TickboxState)))));
+                    }
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Subtract))
+                    {
+                        if (missionPassedScreen.Items.Count > 0)
+                        {
+                            missionPassedScreen.Items.RemoveAt(missionPassedScreen.Items.Count - 1);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
+
---
+++ Source/Elements/MissionPassedScreen.cs
@@ -0,0 +1,346 @@
+namespace RAGENativeUI.Elements
+{
+    using System;
+    using System.Drawing;
+
+    using Rage;
+    using Rage.Native;
+
+    using RAGENativeUI.Utility;
+
+    public class MissionPassedScreen : IDisposable
+    {
+        public delegate void MissionPassedScreenContinuedEventHandler(MissionPassedScreen sender);
+
+        private const int MaxItemsOnScreenCount = 14;
+
+        public BigMessage BigMessage { get; private set; }
+        public InstructionalButtons InstructionalButtons { get; private set; }
+
+        private int instructionalButtonContinueIndex, instructionalButtonExpandIndex;
+
+        private State state;
+
+        public GameControl ContinueControl
+        {
+            get { return (InstructionalButtons.Slots[instructionalButtonContinueIndex] as InstructionalButtonControlSlot).Control; }
+            set { (InstructionalButtons.Slots[instructionalButtonContinueIndex] as InstructionalButtonControlSlot).Control = value; }
+        }
+
+        public GameControl ExpandControl
+        {
+            get { return (InstructionalButtons.Slots[instructionalButtonExpandIndex] as InstructionalButtonControlSlot).Control; }
+            set { (InstructionalButtons.Slots[instructionalButtonExpandIndex] as InstructionalButtonControlSlot).Control = value; }
+        }
+        
+        private string title;
+        /// <exception cref="ArgumentNullException">When setting the property to a null value.</exception>
+        public string Title { get { return title; } set { title = value ?? throw new ArgumentNullException($"The mission passed screen {nameof(Title)} can't be null."); } }
+        private string subtitle;
+        /// <exception cref="ArgumentNullException">When setting the property to a null value.</exception>
+        public string Subtitle { get { return subtitle; } set { subtitle = value ?? throw new ArgumentNullException($"The mission passed screen {nameof(Subtitle)} can't be null."); } }
+        private MissionPassedScreenItemsCollection items;
+        /// <exception cref="ArgumentNullException">When setting the property to a null value.</exception>
+        public MissionPassedScreenItemsCollection Items { get { return items; } set { items = value ?? throw new ArgumentNullException($"The mission passed screen {nameof(Items)} can't be null."); } }
+
+        public bool IsVisible { get { return state != State.None; } }
+
+        public GameFiber Fiber { get; private set; }
+
+        public bool IsCompletionVisible { get; set; }
+        public string CompletionText { get; set; } = Game.GetLocalizedString("MTPHPER"); // Completion
+        public int CompletionPercentage { get; set; } = 100;
+        public Color CompletionMedalColor { get; set; } = HudColor.GOLD.GetColor();
+
+        public string ScreenEffect { get; set; } = "SuccessMichael";
+
+        public event MissionPassedScreenContinuedEventHandler Continued;
+
+        public MissionPassedScreen(string title, string subtitle)
+        {
+            BigMessage = new BigMessage();
+            BigMessage.OutTransition = BigMessage.OutTransitionType.MoveUp;
+            BigMessage.OutTransitionTime = 0.4f;
+            InstructionalButtons = new InstructionalButtons();
+            instructionalButtonContinueIndex = InstructionalButtons.AddSlot(Game.GetLocalizedString("ES_HELP"), GameControl.FrontendEndscreenAccept); // Continue
+            instructionalButtonExpandIndex = InstructionalButtons.AddSlot(Game.GetLocalizedString("ES_XPAND"), GameControl.FrontendEndscreenExpand); // Expand
+
+            Title = title;
+            Subtitle = subtitle;
+            Items = new MissionPassedScreenItemsCollection();
+
+            Fiber = GameFiber.StartNew(UpdateLoop, $"RAGENativeUI - {nameof(MissionPassedScreen)} Update Fiber");
+        }
+
+        public void Show()
+        {
+            InstructionalButtons.Slots[instructionalButtonExpandIndex].IsVisible = true;
+                                        // 277 hours, I guess nobody will stay with this screen open for that long :P
+            BigMessage.CallMethodAndShow(999999999, "SHOW_MISSION_PASSED_MESSAGE", Title, Subtitle, 100, true, GetBigMessageNumStats(), true);
+            if(ScreenEffect != null)
+            {
+                NativeFunction.Natives.x2206BF9A37B7F724(ScreenEffect, 1000, false); // _START_SCREEN_EFFECT
+            }
+            timer = 0;
+            state = State.WaitForInputClosed;
+        }
+
+        public void Hide()
+        {
+            if (state == State.None || state >= State.CloseRollUp)
+                return;
+
+            if(state >= State.WaitForInputExpanded)
+            {
+                state = State.CloseRollUp;
+            }
+            else if(state >= State.WaitForInputClosed)
+            {
+                timer = 0;
+                state = State.CloseHide;
+            }
+        }
+
+        private void UpdateLoop()
+        {
+            while (true)
+            {
+                GameFiber.Yield();
+                Update();
+            }
+        }
+
+        private uint timer;
+        private void Update()
+        {
+            if (state == State.None)
+                return;
+            
+            Draw();
+
+            switch (state)
+            {
+                case State.WaitForInputClosed:
+                    {
+                        if (Game.IsControlJustPressed(0, ExpandControl))
+                        {
+                            InstructionalButtons.Slots[instructionalButtonExpandIndex].IsVisible = false;
+                            state = State.StartExpand;
+                        }
+                        else if (Game.IsControlJustPressed(0, ContinueControl))
+                        {
+                            Hide();
+                        }
+                        break;
+                    }
+
+                case State.StartExpand:
+                    {
+                        BigMessage.CallMethod("TRANSITION_UP", 0.15f, true);
+                        timer = Game.GameTime;
+                        state = State.ExpandRollDown;
+                        break;
+                    }
+
+                case State.ExpandRollDown:
+                    {
+                        if (Game.GameTime > timer + 150)
+                        {
+                            BigMessage.CallMethod("ROLL_DOWN_BACKGROUND");
+                            state = State.WaitForInputExpanded;
+                        }
+
+                        break;
+                    }
+
+                case State.WaitForInputExpanded:
+                    {
+                        if (Game.IsControlJustPressed(0, ContinueControl))
+                        {
+                            Hide();
+                        }
+                        break;
+                    }
+
+                case State.CloseRollUp:
+                    {
+                        BigMessage.CallMethod("ROLL_UP_BACKGROUND");
+                        timer = Game.GameTime;
+                        state = State.CloseHide;
+                        break;
+                    }
+
+                case State.CloseHide:
+                    {
+                        if (Game.GameTime > timer + 200)
+                        {
+                            BigMessage.Hide();
+                            timer = Game.GameTime;
+                            state = State.WaitHide;
+                        }
+                        break;
+                    }
+
+                case State.WaitHide:
+                    {
+                        if (Game.GameTime > timer + 400)
+                        {
+                            state = State.Continue;
+                        }
+                        break;
+                    }
+
+                case State.Continue:
+                    {
+                        OnContinued();
+                        BigMessage.Dismiss();
+                        InstructionalButtons.Dismiss();
+                        state = State.None;
+                        break;
+                    }
+            }
+        }
+
+        private void Draw()
+        {
+            BigMessage.Draw();
+            InstructionalButtons.Draw();
+
+            if(state == State.WaitForInputExpanded)
+            {
+                Color white = HudColor.WHITE.GetColor();
+                Rectangle.Draw(GameScreenRectangle.FromRelativeCoords(0.5f, 0.3275f, 0.27f, 0.0009259259f), white);
+
+                // TODO: show items as the background rolls down, instead of showing them at once
+                float y = 0.3387495f;
+                for (int i = 0; i < Math.Min(Items.Count, MaxItemsOnScreenCount - (IsCompletionVisible ? 1 : 0)); i++)
+                {
+                    MissionPassedScreenItem item = Items[i];
+                    Text.Draw(GameScreenPosition.FromRelativeCoords(0.37f, y), item.Label, 0.35f, white, TextFont.ChaletLondon, TextAlignment.Left, 0f, false, false);
+                    if (item.Tickbox == MissionPassedScreenItem.TickboxState.None)
+                    {
+                        Text.Draw(GameScreenPosition.FromRelativeCoords(0.63f, y), item.Status, 0.35f, white, TextFont.ChaletLondon, TextAlignment.Right, 0f, false, false);
+                    }
+                    else
+                    {
+                        Text.Draw(GameScreenPosition.FromRelativeCoords(0.614f, y), item.Status, 0.35f, white, TextFont.ChaletLondon, TextAlignment.Right, 0f, false, false);
+
+                        const float w = 0.00078125f * 24f * 2f * 0.65f;
+                        const float h = 0.001388889f * 24f * 2f * 0.65f;
+                        float spriteY = y + 0.014f;
+                        Sprite.Draw("commonmenu", GetTickboxTextureName(item.Tickbox), GameScreenRectangle.FromRelativeCoords(0.622f, spriteY, w, h), 0f, Color.White);
+                    }
+                    y += 125f * 0.2f * 0.001388889f;
+                }
+
+                y += 27.5f * 0.2f * 0.001388889f;
+
+                Rectangle.Draw(GameScreenRectangle.FromRelativeCoords(0.5f, y, 0.27f, 0.0009259259f), white);
+
+                if (IsCompletionVisible)
+                {
+                    y += 42f * 0.2f * 0.001388889f;
+
+                    if (CompletionText != null)
+                    {
+                        Text.Draw(GameScreenPosition.FromRelativeCoords(0.4175f, y), CompletionText, 0.4f, white, TextFont.ChaletLondon, TextAlignment.Left, 0f, false, false);
+                    }
+                    Text.Draw(GameScreenPosition.FromRelativeCoords(0.56895f, y), $"{CompletionPercentage}%", 0.395f, white, TextFont.ChaletLondon, TextAlignment.Right, 0f, false, false);
+
+                    y += 0.015715f;
+
+                    const float w = 0.00078125f * 16f * 2f * 0.65f;
+                    const float h = 0.001388889f * 16f * 2f * 0.65f;
+                    Sprite.Draw("mphud", "missionpassedmedal", GameScreenRectangle.FromRelativeCoords(0.57875f, y, w, h), 0f, CompletionMedalColor);
+                }
+            }
+        }
+
+        private void OnContinued()
+        {
+            Continued?.Invoke(this);
+        }
+
+        private int GetBigMessageNumStats()
+        {
+            int count = Math.Min(Items.Count + (IsCompletionVisible ? 1 : 0), 13);
+
+            return count;
+        }
+
+        private string GetTickboxTextureName(MissionPassedScreenItem.TickboxState tickbox)
+        {
+            switch (tickbox)
+            {
+                case MissionPassedScreenItem.TickboxState.Empty: return "shop_box_blank";
+                case MissionPassedScreenItem.TickboxState.Tick: return "shop_box_tick";
+                case MissionPassedScreenItem.TickboxState.Cross: return "shop_box_cross";
+            }
+
+            return null;
+        }
+
+        public void Dispose()
+        {
+            Fiber?.Abort();
+            Fiber = null;
+            BigMessage?.Dismiss();
+            BigMessage = null;
+            InstructionalButtons?.Dismiss();
+            InstructionalButtons = null;
+        }
+
+        private enum State
+        {
+            None,
+
+            WaitForInputClosed,
+
+            StartExpand,
+            ExpandRollDown,
+
+            WaitForInputExpanded,
+
+            CloseRollUp,
+            CloseHide,
+            WaitHide,
+
+            Continue,
+        }
+    }
+
+    public class MissionPassedScreenItem
+    {
+        public enum TickboxState
+        {
+            None,
+            Empty,
+            Tick,
+            Cross,
+        }
+
+        public string Label { get; set; }
+        public string Status { get; set; }
+        public TickboxState Tickbox { get; set; }
+
+        public MissionPassedScreenItem(string label, string status, TickboxState tickbox)
+        {
+            Label = label;
+            Status = status;
+            Tickbox = tickbox;
+        }
+
+        public MissionPassedScreenItem(string label, string status) : this(label, status, TickboxState.None)
+        {
+        }
+
+        public MissionPassedScreenItem(string label) : this(label, "", TickboxState.None)
+        {
+        }
+    }
+
+    public class MissionPassedScreenItemsCollection : BaseCollection<MissionPassedScreenItem>
+    {
+    }
+}
+
