02b2467bc29c5278d1eee6080a11c0302e25ad5b
alexguirre <alexguirre.aa@gmail.com>
2017-07-31 17:02:43 +0200

    Fix access modifiers in FrontendSound

--- Source/Utility/FrontendSound.cs
+++ Source/Utility/FrontendSound.cs
@@ -4,9 +4,9 @@ namespace RAGENativeUI.Utility
 
     public class FrontendSound
     {
-        public string set;
+        private string set;
         public string Set { get { return set; } set { set = value ?? throw new ArgumentNullException($"The frontend sound {nameof(Set)} can't be null."); } }
-        public string name;
+        private string name;
         public string Name { get { return name; } set { name = value ?? throw new ArgumentNullException($"The frontend sound {nameof(Name)} can't be null."); } }
 
         public FrontendSound(string set, string name)
