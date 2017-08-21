d7b3080eefb756aef9d6b016c6dbcd1b59f3c755
alexguirre <alexguirre.aa@gmail.com>
2017-08-22 01:02:44 +0200

    Add start year and current year to versioning script

--- PreBuild/versioning.ps1
+++ PreBuild/versioning.ps1
@@ -5,7 +5,14 @@ $MINOR_VERSION = 0;
 $COMMITS_COUNT = git rev-list HEAD --count;
 $BUILDS_COUNT = 0;
 $EXTRA_INFO = "PRE-RELEASE";
-$YEAR = [System.DateTime]::UtcNow.Year;
+$START_YEAR = 2016;
+$CURRENT_YEAR = [System.DateTime]::UtcNow.Year;
+
+$YEAR_TEXT = $START_YEAR.ToString();
+if($START_YEAR -ne $CURRENT_YEAR)
+{
+    $YEAR_TEXT += ("-" + $CURRENT_YEAR.ToString());
+}
 
 if($config.ToLower() -eq "release")
 {
@@ -31,7 +38,7 @@ $newAssemblyText = Get-Content $templateFile |
     %{$_ -replace '\$COMMITS_COUNT\$', ($COMMITS_COUNT) } |
     %{$_ -replace '\$BUILDS_COUNT\$', ($BUILDS_COUNT) } |
     %{$_ -replace '\$EXTRA_INFO\$', ($EXTRA_INFO) } |
-    %{$_ -replace '\$YEAR\$', ($YEAR) };
+    %{$_ -replace '\$YEAR\$', ($YEAR_TEXT) };
 
 $newAssemblyText > $destinationFile;
 
