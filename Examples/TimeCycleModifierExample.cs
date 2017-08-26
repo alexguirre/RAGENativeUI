6b2e5272d3eb6149d509748ce65f39c330429a66
alexguirre <alexguirre.aa@gmail.com>
2017-08-26 14:45:59 +0200

    Use ImGui in TimeCycleModifiersExample

--- Examples/TimeCycleModifierExample.cs
+++ Examples/TimeCycleModifierExample.cs
@@ -1,12 +1,14 @@
 namespace Examples
 {
     using System;
+    using System.Drawing;
 
     using Rage;
     using Rage.Attributes;
 
     using RAGENativeUI;
     using RAGENativeUI.Elements;
+    using RAGENativeUI.ImGui;
 
     internal static class TimeCycleModifierExample
     {
@@ -16,117 +18,98 @@ namespace Examples
             int i = 0;
             TimeCycleModifier mod = TimeCycleModifier.GetByIndex(i);
 
-            GameFiber.StartNew(() =>
+            RectangleF mainWindowRect = new RectangleF(10, 10, 350, 500);
+            RectangleF modsValuesWindowRect = new RectangleF(Game.Resolution.Width - 320, 10, 310, 1000);
+            bool showCurrentModsValues = false;
+
+            Gui.Do += () =>
             {
-                while (true)
+                Gui.Mouse();
+                mainWindowRect = Gui.Window(mainWindowRect, "Time Cycle Modifiers Example");
+
+                if(Gui.Button(new RectangleF(5, 3, 122, 25), "Previous"))
                 {
-                    GameFiber.Yield();
+                    i--;
+                    if (i < 0)
+                        i = TimeCycleModifier.Count - 1;
+                    TimeCycleModifier.CurrentModifier = null;
+                    mod = TimeCycleModifier.GetByIndex(i);
+                }
 
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.H))
-                    {
-                        foreach (TimeCycleModifier e in TimeCycleModifier.GetAll())
-                        {
-                            Game.Console.Print($"{e.Index}. {e.Name}");
-                        }
-                    }
+                if (Gui.Button(new RectangleF(5 + 3 + 122, 3, 122, 25), "Next"))
+                {
+                    i++;
+                    if (i >= TimeCycleModifier.Count)
+                        i = 0;
+                    TimeCycleModifier.CurrentModifier = null;
+                    mod = TimeCycleModifier.GetByIndex(i);
+                }
 
-                    float y = 0.005f;
-                    Text.Draw(ScreenPosition.FromRelativeCoords(0.98f, y), $"Mods Count: {mod.Mods.Count}", 0.45f, System.Drawing.Color.White, TextFont.ChaletLondon, TextAlignment.Right, 0.0f, false, true);
-                    y += 0.027f;
+                TimeCycleModifier current = TimeCycleModifier.CurrentModifier;
+                Gui.Label(new RectangleF(5, 30, 350, 20), $"Current");
+                Gui.Label(new RectangleF(15, 30 + 25 * 1, 350, 20), $"Name: {mod.Name}");
+                Gui.Label(new RectangleF(15, 30 + 25 * 2, 350, 20), $"Index: {mod.Index}");
+                Gui.Label(new RectangleF(15, 30 + 25 * 3, 350, 20), $"Flags: {mod.Flags}");
+                Gui.Label(new RectangleF(15, 30 + 25 * 4, 350, 20), $"Active: {mod.IsActive}");
+                Gui.Label(new RectangleF(15, 30 + 25 * 5, 350, 20), $"Memory Address: {mod.MemoryAddress.ToString("X")}");
+
+                Gui.Label(new RectangleF(5, 30 + 25 * 7, 350, 20), $"Current Strength: {TimeCycleModifier.CurrentModifierStrength}");
+                Gui.Label(new RectangleF(5, 30 + 25 * 8, 350, 20), $"Total Count: {TimeCycleModifier.Count}");
+
+                if (Gui.Button(new RectangleF(5, 30 * 10, 250, 25), mod.IsActive ? "Deactivate Current" : "Activate Current"))
+                {
+                    mod.IsActive = !mod.IsActive;
+                }
+
+                if (Gui.Button(new RectangleF(5, 30 * 11, 250, 25),  $"{(showCurrentModsValues ? "Hide" : "Show")} Current Mods Values"))
+                {
+                    showCurrentModsValues = !showCurrentModsValues;
+                }
+
+                if (Gui.Button(new RectangleF(5, 30 * 12, 250, 25), "Randomize Current Mods Values"))
+                {
                     foreach (TimeCycleModifierMod m in mod.Mods)
                     {
-                        string s = $"{m.Type}: {m.Value1.ToString("0.000")} {m.Value2.ToString("0.000")}~n~";
-                        Text.Draw(ScreenPosition.FromRelativeCoords(0.98f, y), s, 0.3f, System.Drawing.Color.White, TextFont.ChaletLondon, TextAlignment.Right, 0.0f, false, true);
-                        y += 0.0165f;
-                    }
-
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.U))
-                    {
-                        foreach (TimeCycleModifierMod m in mod.Mods)
-                        {
-                            m.Value1 = MathHelper.GetRandomSingle(0.0f, 15.0f);
-                            m.Value2 = MathHelper.GetRandomSingle(0.0f, 15.0f);
-                        }
-                    }
-
-                    TimeCycleModifier current = TimeCycleModifier.CurrentModifier;
-                    Game.DisplayHelp($"Name: {mod.Name}~n~Index: {mod.Index}~n~Flags: {mod.Flags}~n~Active: {mod.IsActive.ToString()}~n~Valid: {mod.IsValid()}~n~MemAddress: {mod.MemoryAddress.ToString("X")}~n~Current: {(current == null ? "null" : current.Name)}~n~CurrentIndex: {(current == null ? "-1" : current.Index.ToString())}~n~Strength: {TimeCycleModifier.CurrentModifierStrength}~n~Count: {TimeCycleModifier.Count}");
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Y))
-                    {
-                        mod.IsActive = !mod.IsActive;
-                    }
-
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Add))
-                    {
-                        i++;
-                        if (i >= TimeCycleModifier.Count)
-                            i = 0;
-                        TimeCycleModifier.CurrentModifier = null;
-                        mod = TimeCycleModifier.GetByIndex(i);
-                    }
-                    else if (Game.IsKeyDown(System.Windows.Forms.Keys.Subtract))
-                    {
-                        i--;
-                        if (i < 0)
-                            i = TimeCycleModifier.Count - 1;
-                        TimeCycleModifier.CurrentModifier = null;
-                        mod = TimeCycleModifier.GetByIndex(i);
-                    }
-
-
-                    if (Game.IsKeyDownRightNow(System.Windows.Forms.Keys.Multiply))
-                    {
-                        TimeCycleModifier.CurrentModifierStrength += 1.0f * Game.FrameTime;
-                    }
-                    else if (Game.IsKeyDownRightNow(System.Windows.Forms.Keys.Divide))
-                    {
-                        TimeCycleModifier.CurrentModifierStrength -= 1.0f * Game.FrameTime;
-                    }
-
-
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.J))
-                    {
-                        Game.DisplayNotification("Creating new CUSTOMCLONE" + TimeCycleModifier.Count);
-                        Game.LogTrivial("Creating new CUSTOMCLONE" + TimeCycleModifier.Count);
-                        System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-                        TimeCycleModifier m = new TimeCycleModifier("CUSTOMCLONE" + TimeCycleModifier.Count, mod);
-                        sw.Stop();
-                        Game.LogTrivial($"It took {sw.ElapsedMilliseconds}ms/{sw.ElapsedTicks}ticks/{sw.Elapsed} to create TimeCycleModifier");
-                    }
-                    else if (Game.IsKeyDown(System.Windows.Forms.Keys.I))
-                    {
-                        Game.DisplayNotification("Creating new CUSTOM" + TimeCycleModifier.Count);
-                        Game.LogTrivial("Creating new CUSTOM" + TimeCycleModifier.Count);
-                        System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-                        TimeCycleModifier m = new TimeCycleModifier("CUSTOM" + TimeCycleModifier.Count, 0, 
-                           Tuple.Create(TimeCycleModifierModType.postfx_desaturation, MathHelper.GetRandomSingle(0.0f, 50.0f), 0.0f),
-                           Tuple.Create(TimeCycleModifierModType.postfx_bright_pass_thresh, MathHelper.GetRandomSingle(0.0f, 50.0f), 0.0f));
-                        sw.Stop();
-                        Game.LogTrivial($"It took {sw.ElapsedMilliseconds}ms/{sw.ElapsedTicks}ticks/{sw.Elapsed} to create TimeCycleModifier");
-                    }
-
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.NumPad5))
-                    {
-                        if (mod.Mods.Has(TimeCycleModifierModType.screen_blur_intensity))
-                        {
-                            mod.Mods.Remove(TimeCycleModifierModType.screen_blur_intensity);
-                        }
-                        else
-                        {
-                            mod.Mods.Add(TimeCycleModifierModType.screen_blur_intensity, 5.0f, 0.0f);
-                        }
-
-                        if (mod.Mods.Has(TimeCycleModifierModType.light_dynamic_bake_tweak))
-                        {
-                            mod.Mods.Remove(TimeCycleModifierModType.light_dynamic_bake_tweak);
-                        }
-                        else
-                        {
-                            mod.Mods.Add(TimeCycleModifierModType.light_dynamic_bake_tweak, 1.0f, 0.0f);
-                        }
+                        m.Value1 = MathHelper.GetRandomSingle(0.0f, 15.0f);
+                        m.Value2 = MathHelper.GetRandomSingle(0.0f, 15.0f);
                     }
                 }
-            });
+
+                if (Gui.Button(new RectangleF(5, 30 * 13, 250, 25), "Clone Current"))
+                {
+                    Game.DisplayNotification("Creating new CLONE" + TimeCycleModifier.Count);
+                    Game.LogTrivial("Creating new CLONE" + TimeCycleModifier.Count);
+                    TimeCycleModifier m = new TimeCycleModifier("CLONE" + TimeCycleModifier.Count, mod);
+                }
+
+                if (Gui.Button(new RectangleF(5, 30 * 14, 250, 25), "New Modifier"))
+                {
+                    Game.DisplayNotification("Creating new CUSTOM" + TimeCycleModifier.Count);
+                    Game.LogTrivial("Creating new CUSTOM" + TimeCycleModifier.Count);
+                    TimeCycleModifier m = new TimeCycleModifier("CUSTOM" + TimeCycleModifier.Count, 0,
+                       Tuple.Create(TimeCycleModifierModType.postfx_desaturation, MathHelper.GetRandomSingle(0.0f, 50.0f), 0.0f),
+                       Tuple.Create(TimeCycleModifierModType.postfx_bright_pass_thresh, MathHelper.GetRandomSingle(0.0f, 50.0f), 0.0f));
+                }
+            };
+
+            Gui.Do += () =>
+            {
+                if (showCurrentModsValues)
+                {
+                    Gui.Mouse();
+                    modsValuesWindowRect = Gui.Window(modsValuesWindowRect, "Mods Values");
+
+                    Gui.Label(new RectangleF(3, 3, 300, 25), $"Mods Count: {mod.Mods.Count}");
+
+                    float y = 30;
+                    foreach (TimeCycleModifierMod m in mod.Mods)
+                    {
+                        string s = $"{m.Type}: {m.Value1.ToString("0.000")} {m.Value2.ToString("0.000")}";
+                        Gui.Label(new RectangleF(3, y, 300, 25), s);
+                        y += 26;
+                    }
+                }
+            };
         }
     }
 }
