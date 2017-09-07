ebf52378a3b54dd6a1087dd39c8d8f4afb326fb7
alexguirre <alexguirre.aa@gmail.com>
2017-09-07 16:40:27 +0200

    Improve Control.IsHeld()

--- Source/Control.cs
+++ Source/Control.cs
@@ -1,5 +1,8 @@
 namespace RAGENativeUI
 {
+    using System;
+    using System.Linq;
+    using System.Collections.ObjectModel;
     using System.Windows.Forms;
 
     using Rage;
@@ -7,18 +10,25 @@ namespace RAGENativeUI
 
     public class Control
     {
+        // used if HeldTimeSteps is null or empty
+        private const uint DefaultHeldCooldown = 180;
+
         public Keys? Key { get; set; }
         public ControllerButtons? Button { get; set; }
         public GameControl? NativeControl { get; set; }
-        public uint HeldCooldown { get; set; } = 175;
+        public TimeStep[] HeldTimeSteps { get; set; }
 
-        protected uint NextHeldTime { get; set; }
+        private uint heldStartTime;
+        private int currentHeldStepIndex;
+        private uint heldCooldown;
+        private uint nextHeldTime;
 
         public Control(Keys? key = null, ControllerButtons? button = null, GameControl? nativeControl = null)
         {
             Key = key;
             Button = button;
             NativeControl = nativeControl;
+            HeldTimeSteps = DefaultHeldTimeSteps.ToArray();
         }
 
         public Control(GameControl? nativeControl = null) : this(null, null, nativeControl)
@@ -36,7 +46,7 @@ namespace RAGENativeUI
             if (NativeControl.HasValue && (NativeFunction.Natives.IsControlJustPressed<bool>(0, (int)NativeControl.Value) || NativeFunction.Natives.IsDisabledControlJustPressed<bool>(0, (int)NativeControl.Value)))
                 return true;
 
-            NextHeldTime = 0;
+            ResetHeld();
             return false;
         }
 
@@ -51,17 +61,19 @@ namespace RAGENativeUI
             if (NativeControl.HasValue && (NativeFunction.Natives.IsControlPressed<bool>(0, (int)NativeControl.Value) || NativeFunction.Natives.IsDisabledControlPressed<bool>(0, (int)NativeControl.Value)))
                 return true;
 
-            NextHeldTime = 0;
+            ResetHeld();
             return false;
         }
 
+        // returns true every X time(defined by the HeldTimeSteps) while the key/button/control is pressed, instead of returning true every tick like IsPressed
+        // TODO: maybe rename IsHeld to better reflect its behaviour
         public bool IsHeld()
         {
-            if(Game.GameTime <= NextHeldTime)
+            if(Game.GameTime <= nextHeldTime)
             {
                 if(!IsPressed())
                 {
-                    NextHeldTime = 0;
+                    ResetHeld();
                 }
 
                 return false;
@@ -69,12 +81,62 @@ namespace RAGENativeUI
 
             if (IsPressed())
             {
-                NextHeldTime = Game.GameTime + HeldCooldown;
+                if (heldStartTime == 0)
+                {
+                    heldStartTime = Game.GameTime;
+                }
+                UpdateHeldStep();
+                nextHeldTime = Game.GameTime + heldCooldown;
                 return true;
             }
 
-            NextHeldTime = 0;
+            ResetHeld();
             return false;
+        }
+
+        private void UpdateHeldStep()
+        {
+            if (HeldTimeSteps != null && heldStartTime != 0 && HeldTimeSteps.Length > 0)
+            {
+                if (currentHeldStepIndex != HeldTimeSteps.Length - 1)
+                {
+                    int newIndex = currentHeldStepIndex + 1;
+                    if ((Game.GameTime - heldStartTime) >= HeldTimeSteps[newIndex].Time)
+                        currentHeldStepIndex = newIndex;
+                }
+
+                heldCooldown = HeldTimeSteps[currentHeldStepIndex].HeldCooldown;
+            }
+            else
+            {
+                heldCooldown = DefaultHeldCooldown;
+            }
+        }
+
+        private void ResetHeld()
+        {
+            heldStartTime = 0;
+            currentHeldStepIndex = 0;
+            nextHeldTime = 0;
+        }
+
+        public static readonly ReadOnlyCollection<TimeStep> DefaultHeldTimeSteps = Array.AsReadOnly(new[]
+        {
+            new TimeStep(0, 180),
+            new TimeStep(2000, 110),
+            new TimeStep(6000, 50),
+        });
+
+        public struct TimeStep
+        {
+            public uint Time { get; }
+            public uint HeldCooldown { get; }
+
+            public TimeStep(uint time, uint heldCooldown)
+            {
+                Time = time;
+                HeldCooldown = heldCooldown;
+            }
         }
     }
 }
