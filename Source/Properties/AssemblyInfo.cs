2f586313de6f6c7a2a0af5a293f8fc6ba910ebe6
alexguirre <alexguirre.aa@gmail.com>
2017-10-09 16:53:58 +0200

    Switch to AppVeyor for versioning and automated builds

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RAGENativeUI")]
+[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("alexguirre")]
+[assembly: AssemblyProduct("RAGENativeUI")]
+[assembly: AssemblyCopyright("Copyright ©  2017 alexguirre")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fc3d6fad-db94-4c47-9b48-3b783398bdc5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0")]
+
