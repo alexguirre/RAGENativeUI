bbac385aeb8fc2c429f621b4f48aadf7cdd189f3
alexguirre <alexguirre.aa@gmail.com>
2019-01-20 16:12:59 +0100

    Set proper ProgressTimerBar constants

--- Source/RAGENativeUI/TimerBars/ProgressTimerBar.cs
+++ Source/RAGENativeUI/TimerBars/ProgressTimerBar.cs
@@ -35,35 +35,22 @@ namespace RAGENativeUI.TimerBars
 
             base.Draw(index);
 
-            // TODO: search for the actual ProgressTimerBar constants value in the game scripts
-            const float YOffset = 0.008f;
-            const float Width = 0.059f;
+            // TODO: ProgressTimerBar background should be lower
+
+            // Constants from the game scripts
+            const float X = (((((0.919f - 0.081f) + 0.028f) + 0.05f) - 0.001f) - 0.002f);
+            const float YOffset = ((((0.013f - 0.002f) + 0.001f) + 0.001f) - 0.001f);
+            const float Width = 0.069f;
             const float Height = 0.011f;
 
             Vector2 pos = Position(index);
-            pos.X = (((((0.95f - 0.047f) + 0.001f) + 0.047f) - 0.002f) + 0.001f) - Width * 0.5f;
+            pos.X = X;
             pos.Y += YOffset;
 
             N.DrawRect(pos.X, pos.Y, Width, Height, BackColor.R, BackColor.G, BackColor.B, BackColor.A);
 
             pos.X = pos.X - Width * 0.5f + Width * 0.5f * percentage;
             N.DrawRect(pos.X, pos.Y, Width * percentage, Height, ForeColor.R, ForeColor.G, ForeColor.B, ForeColor.A);
-
-
-
-
-            //Vector2 pos = Position;
-            //Vector2 size = Size;
-
-            //float barX = pos.X + 0.04f;
-            //float barY = pos.Y;
-            //float barW = size.X / 2.25f;
-            //float barH = size.Y / 3f;
-            //Rect.Draw((barX, barY).Rel(), (barW, barH).Rel(), BackColor);
-
-            //float fillX = barX - barW * 0.5f + barW * 0.5f * percentage;
-            //float fillW = barW * percentage;
-            //Rect.Draw((fillX, barY).Rel(), (fillW, barH).Rel(), ForeColor);
         }
     }
 }
