6e95c17b1a208594ae3f9eb6d4695e7802a2e21d
alexguirre <alexguirre.aa@gmail.com>
2020-04-20 22:25:42 +0200

    (#64) Rename CheckpointsTimerBar.Checkpoint to TimerBarCheckpoint and CheckpointsTimerBar.CheckpointState to TimerBarCheckpointState

--- Source/Elements/TimerBars/CheckpointsTimerBar.cs
+++ Source/Elements/TimerBars/CheckpointsTimerBar.cs
@@ -11,25 +11,25 @@
     /// Additionally, it can be crossed out.
     /// </para>
     /// <para>
-    /// The state of the checkpoints is defined using the <see cref="Checkpoint"/> class and the <see cref="Checkpoints"/> property.
+    /// The state of the checkpoints is defined using the <see cref="TimerBarCheckpoint"/> class and the <see cref="Checkpoints"/> property.
     /// </para>
     /// </summary>
-    /// <see cref="Checkpoint"/>
-    /// <see cref="CheckpointState"/>
+    /// <see cref="TimerBarCheckpoint"/>
+    /// <see cref="TimerBarCheckpointState"/>
     public class CheckpointsTimerBar : TimerBarBase
     {
         /// <summary>
-        /// Gets the list containing the <see cref="Checkpoint"/>s of the timer bar.
+        /// Gets the list containing the <see cref="TimerBarCheckpoint"/>s of the timer bar.
         /// The checkpoints are shown from right to left: the checkpoint at index 0 is the right-most circle
         /// and the last checkpoint in the list is the left-most circle.
         /// </summary>
-        public IList<Checkpoint> Checkpoints { get; }
+        public IList<TimerBarCheckpoint> Checkpoints { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CheckpointsTimerBar"/> class.
         /// </summary>
         /// <param name="label">A <see cref="string"/> that will appear at the left side of the timer bar.</param>
-        /// <param name="numberOfCheckpoints">Determines the number of <see cref="Checkpoint"/>s created initially.</param>
+        /// <param name="numberOfCheckpoints">Determines the number of <see cref="TimerBarCheckpoint"/>s created initially.</param>
         /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="numberOfCheckpoints"/> is negative.</exception>
         public CheckpointsTimerBar(string label, int numberOfCheckpoints) : base(label)
         {
@@ -38,10 +38,10 @@
                 throw new ArgumentOutOfRangeException(nameof(numberOfCheckpoints), numberOfCheckpoints, $"{nameof(numberOfCheckpoints)} is negative");
             }
 
-            Checkpoints = new List<Checkpoint>();
+            Checkpoints = new List<TimerBarCheckpoint>();
             for (int i = 0; i < numberOfCheckpoints; i++)
             {
-                Checkpoints.Add(new Checkpoint());
+                Checkpoints.Add(new TimerBarCheckpoint());
             }
         }
 
@@ -56,14 +56,14 @@
                 y += TB.CheckpointYOffset;
 
                 bool requestedCrossTxd = false;
-                foreach (Checkpoint cp in Checkpoints)
+                foreach (TimerBarCheckpoint cp in Checkpoints)
                 {
                     if (cp != null)
                     {
                         Color circleColor = cp.State switch
                         {
-                            CheckpointState.Completed => cp.Color,
-                            CheckpointState.Failed => Color.Black,
+                            TimerBarCheckpointState.Completed => cp.Color,
+                            TimerBarCheckpointState.Failed => Color.Black,
                             _ => Color.FromArgb(51, 255, 255, 255),
                         };
 
@@ -83,7 +83,7 @@
                                 requestedCrossTxd = true;
                             }
 
-                            int crossColor = cp.State == CheckpointState.Failed ? 255 : 0;
+                            int crossColor = cp.State == TimerBarCheckpointState.Failed ? 255 : 0;
                             N.DrawSprite(TB.CrossTextureDictionary, TB.CrossTextureName, x, y, TB.CheckpointWidth, TB.CheckpointHeight, 0.0f,
                                         crossColor, crossColor, crossColor, 250);
                         }
@@ -93,64 +93,64 @@
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// Specifies the state of a <see cref="TimerBarCheckpoint"/>.
+    /// </summary>
+    public enum TimerBarCheckpointState
+    {
+        /// <summary>
+        /// Represents an unfinished objective. Shown as a grey circle.
+        /// </summary>
+        InProgress = 0,
+        /// <summary>
+        /// Represents a successfully completed objective. Shown as a circle with color <see cref="TimerBarCheckpoint.Color"/>.
+        /// </summary>
+        Completed,
+        /// <summary>
+        /// Represents a failed objective. Shown as a black circle.
+        /// </summary>
+        Failed,
+    }
+
+    /// <summary>
+    /// Represents a checkpoint of a <see cref="CheckpointsTimerBar"/>.
+    /// </summary>
+    public class TimerBarCheckpoint
+    {
+        /// <summary>
+        /// Gets or sets the current state of this checkpoint.
+        /// </summary>
+        public TimerBarCheckpointState State { get; set; }
 
         /// <summary>
-        /// Specifies the state of a <see cref="Checkpoint"/>.
+        /// Gets or sets whether the checkpoint is crossed out.
+        /// If <c>true</c>, a cross is drawn on top of the checkpoint circle, otherwise, nothing is drawn on top.
         /// </summary>
-        public enum CheckpointState
+        public bool IsCrossedOut { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color used to draw the checkpoint when <see cref="State"/> is set to <see cref="TimerBarCheckpointState.Completed"/>.
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimerBarCheckpoint"/> class with the given color.
+        /// </summary>
+        /// <param name="color">The color used to draw the checkpoint when <see cref="State"/> is set to <see cref="TimerBarCheckpointState.Completed"/></param>
+        public TimerBarCheckpoint(Color color)
         {
-            /// <summary>
-            /// Represents an unfinished objective. Shown as a grey circle.
-            /// </summary>
-            InProgress = 0,
-            /// <summary>
-            /// Represents a successfully completed objective. Shown as a circle with color <see cref="Checkpoint.Color"/>.
-            /// </summary>
-            Completed,
-            /// <summary>
-            /// Represents a failed objective. Shown as a black circle.
-            /// </summary>
-            Failed,
+            State = TimerBarCheckpointState.InProgress;
+            IsCrossedOut = false;
+            Color = color;
         }
 
         /// <summary>
-        /// Represents a checkpoint of a <see cref="CheckpointsTimerBar"/>.
+        /// Initializes a new instance of the <see cref="TimerBarCheckpoint"/> class with <see cref="Color"/> set to <see cref="Color.White"/>.
         /// </summary>
-        public class Checkpoint
+        public TimerBarCheckpoint() : this(Color.White)
         {
-            /// <summary>
-            /// Gets or sets the current state of this checkpoint.
-            /// </summary>
-            public CheckpointState State { get; set; }
-
-            /// <summary>
-            /// Gets or sets whether the checkpoint is crossed out.
-            /// If <c>true</c>, a cross is drawn on top of the checkpoint circle, otherwise, nothing is drawn on top.
-            /// </summary>
-            public bool IsCrossedOut { get; set; }
-
-            /// <summary>
-            /// Gets or sets the color used to draw the checkpoint when <see cref="State"/> is set to <see cref="CheckpointState.Completed"/>.
-            /// </summary>
-            public Color Color { get; set; }
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Checkpoint"/> class with the given color.
-            /// </summary>
-            /// <param name="color">The color used to draw the checkpoint when <see cref="State"/> is set to <see cref="CheckpointState.Completed"/></param>
-            public Checkpoint(Color color)
-            {
-                State = CheckpointState.InProgress;
-                IsCrossedOut = false;
-                Color = color;
-            }
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Checkpoint"/> class with <see cref="Color"/> set to <see cref="Color.White"/>.
-            /// </summary>
-            public Checkpoint() : this(Color.White)
-            {
-            }
         }
     }
 }
