0c88ad607991be8da74a2b8dc62d5c2a8b9fd284
alexguirre <alexguirre.aa@gmail.com>
2020-05-05 00:33:17 +0200

    (#48) Add UIMenuListScrollerItem docs

--- Source/Elements/MenuItems/UIMenuListScrollerItem.cs
+++ Source/Elements/MenuItems/UIMenuListScrollerItem.cs
@@ -4,17 +4,35 @@
     using System.Linq;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Represents a scroller item with a list of items to scroll through.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the items.
+    /// </typeparam>
     public class UIMenuListScrollerItem<T> : UIMenuScrollerItem
     {
         private IList<T> items;
-        private string selectedValueText;
+        private string selectedItemText;
         private Func<T, string> formatter;
 
-        public T Value
+        /// <summary>
+        /// Gets the item that is currently selected.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        /// <see cref="Items"/> is empty.
+        /// </exception>
+        public T SelectedItem
         {
-            get => IsEmpty ? throw new InvalidOperationException("empty") : Items[Index];
+            get => IsEmpty ? throw new InvalidOperationException($"{nameof(Items)} list is empty") : Items[Index];
         }
 
+        /// <summary>
+        /// Gets or sets the list containing the items.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// <c>value</c> is null.
+        /// </exception>
         public IList<T> Items
         {
             get => items;
@@ -29,11 +47,19 @@
                 {
                     items = value;
 
-                    Index = IsEmpty ? -1 : ((Index == -1 ? 0 : Index) % OptionCount); // in case the new list has a different size
+                    Index = IsEmpty ? EmptyIndex : ((Index == EmptyIndex ? 0 : Index) % OptionCount); // in case the new list has a different size
                 }
             }
         }
 
+        /// <summary>
+        /// Gets or sets the formatter used to display the selected item.
+        /// A <see cref="Func{T, TResult}"/> that takes in a <typeparamref name="T"/> and returns its <see cref="string"/> representation.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// <c>value</c> is null.
+        /// </exception>
+        /// <seealso cref="DefaultFormatter(T)"/>
         public Func<T, string> Formatter
         {
             get => formatter;
@@ -47,23 +73,34 @@
                 if (value != formatter)
                 {
                     formatter = value;
-                    SyncSelectedTextToValue();
+                    SyncSelectedItemText();
                 }
             }
         }
 
         /// <inheritdoc/>
-        public override string OptionText => IsEmpty ? null : selectedValueText;
+        public override string OptionText => IsEmpty ? null : selectedItemText;
 
         /// <inheritdoc/>
         public override int OptionCount => Items.Count;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIMenuListScrollerItem{T}"/> class with the specified items.
+        /// </summary>
+        /// <param name="text">The label of this list scroller.</param>
+        /// <param name="description">The description of this list scroller.</param>
+        /// <param name="items">The items of this list scroller.</param>
         public UIMenuListScrollerItem(string text, string description, IEnumerable<T> items) : base(text, description)
         {
             Formatter = DefaultFormatter;
             Items = new List<T>(items);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIMenuListScrollerItem{T}"/> class with no items.
+        /// </summary>
+        /// <param name="text">The label of this menu item.</param>
+        /// <param name="description">The description of this menu item.</param>
         public UIMenuListScrollerItem(string text, string description) : this(text, description, Enumerable.Empty<T>())
         {
         }
@@ -71,19 +108,28 @@
         /// <inheritdoc/>
         protected override void OnSelectedIndexChanged(int oldIndex, int newIndex)
         {
-            SyncSelectedTextToValue();
+            SyncSelectedItemText();
 
             base.OnSelectedIndexChanged(oldIndex, newIndex);
         }
 
-        private void SyncSelectedTextToValue()
+        private void SyncSelectedItemText()
         {
-            selectedValueText = (Items == null || IsEmpty) ? null : Formatter(Value);
+            selectedItemText = (Items == null || IsEmpty) ? null : Formatter(SelectedItem);
         }
 
+        /// <summary>
+        /// The default value of <see cref="Formatter"/>.
+        /// The returned <see cref="string"/> is obtained through the <see cref="object.ToString"/> method of <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="value">The value to format.</param>
+        /// <returns>
+        /// The <see cref="string"/> representation of <paramref name="value"/>.
+        /// If <paramref name="value"/> is <c>null</c>, <c>null</c> is returned.
+        /// </returns>
         public static string DefaultFormatter(T value)
         {
-            return value.ToString();
+            return value?.ToString();
         }
     }
 }
