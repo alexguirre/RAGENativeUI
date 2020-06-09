b45d70ea368c0bf6cfaa658797bd2d80b78dc9f9
alexguirre <alexguirre.aa@gmail.com>
2020-06-09 03:22:52 +0200

    (#61) Create examples project

---
+++ Source/Examples/Plugin.cs
@@ -0,0 +1,12 @@
+﻿namespace RNUIExamples
+{
+    using Rage;
+
+    public static class Plugin
+    {
+        private static void Main()
+        {
+            Game.LogTrivial("Hello, World!");
+        }
+    }
+}
---
+++ Source/Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Rage.Attributes;
+
+[assembly: AssemblyTitle("RAGENativeUI Examples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RAGENativeUI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fb83ecbc-e996-4314-8c65-e79211aea8bc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Plugin("RAGENativeUI Examples", Author = "alexguirre", PrefersSingleInstance = true, SupportUrl = "https://github.com/alexguirre/ragenativeui")]
