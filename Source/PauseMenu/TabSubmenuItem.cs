1dbff41334e2041fc0c63bb417b225ec64b37ed3
alexguirre <alexguirre.aa@gmail.com>
2020-07-15 13:06:56 +0200

    Prevent TabSubmenuItem and the selected child TabItem from receiving input at the same time

--- Source/PauseMenu/TabSubmenuItem.cs
+++ Source/PauseMenu/TabSubmenuItem.cs
@@ -41,47 +41,50 @@ namespace RAGENativeUI.PauseMenu
 
             if (!Focused) return;
 
-            if (Common.IsDisabledControlJustPressed(0, GameControl.CellphoneSelect) && Focused && Parent.FocusLevel == 1)
+            if (Items.Count > 0 && Items[Index].Focused)
             {
-                Common.PlaySound("SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET");
-
-                if (Items[Index].CanBeFocused && !Items[Index].Focused)
+                if (Common.IsDisabledControlJustPressed(0, GameControl.CellphoneCancel) && Focused && Parent.FocusLevel > 1)
                 {
-                    Parent.FocusLevel++;
-                    Items[Index].JustOpened = true;
-                    Items[Index].Focused = true;
-                }
-                else
-                {
-                    Items[Index].OnActivated();
+                    Common.PlaySound("CANCEL", "HUD_FRONTEND_DEFAULT_SOUNDSET");
+                    if (Items[Index].CanBeFocused && Items[Index].Focused)
+                    {
+                        Parent.FocusLevel--;
+                        Items[Index].Focused = false;
+                    }
                 }
 
-                
+                Items[Index].ProcessControls();
             }
-
-            if (Common.IsDisabledControlJustPressed(0, GameControl.CellphoneCancel) && Focused && Parent.FocusLevel > 1)
+            else
             {
-                Common.PlaySound("CANCEL", "HUD_FRONTEND_DEFAULT_SOUNDSET");
-                if (Items[Index].CanBeFocused && Items[Index].Focused)
+                if (Common.IsDisabledControlJustPressed(0, GameControl.CellphoneSelect) && Focused && Parent.FocusLevel == 1)
                 {
-                    Parent.FocusLevel--;
-                    Items[Index].Focused = false;
+                    Common.PlaySound("SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET");
+
+                    if (Items[Index].CanBeFocused && !Items[Index].Focused)
+                    {
+                        Parent.FocusLevel++;
+                        Items[Index].JustOpened = true;
+                        Items[Index].Focused = true;
+                    }
+                    else
+                    {
+                        Items[Index].OnActivated();
+                    }
+                }
+
+                if (Common.IsDisabledControlJustPressed(0, GameControl.FrontendUp) || Common.IsDisabledControlJustPressed(0, GameControl.MoveUpOnly) || Common.IsDisabledControlJustPressed(0, GameControl.CursorScrollUp) && Parent.FocusLevel == 1)
+                {
+                    Index = (1000 - (1000 % Items.Count) + Index - 1) % Items.Count;
+                    Common.PlaySound("NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET");
+                }
+
+                else if (Common.IsDisabledControlJustPressed(0, GameControl.FrontendDown) || Common.IsDisabledControlJustPressed(0, GameControl.MoveDownOnly) || Common.IsDisabledControlJustPressed(0, GameControl.CursorScrollDown) && Parent.FocusLevel == 1)
+                {
+                    Index = (1000 - (1000 % Items.Count) + Index + 1) % Items.Count;
+                    Common.PlaySound("NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET");
                 }
             }
-
-            if (Common.IsDisabledControlJustPressed(0, GameControl.FrontendUp) || Common.IsDisabledControlJustPressed(0, GameControl.MoveUpOnly) || Common.IsDisabledControlJustPressed(0, GameControl.CursorScrollUp) && Parent.FocusLevel == 1)
-            {
-                Index = (1000 - (1000 % Items.Count) + Index - 1) % Items.Count;
-                Common.PlaySound("NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET");
-            }
-
-            else if (Common.IsDisabledControlJustPressed(0, GameControl.FrontendDown) || Common.IsDisabledControlJustPressed(0, GameControl.MoveDownOnly) || Common.IsDisabledControlJustPressed(0, GameControl.CursorScrollDown) && Parent.FocusLevel == 1)
-            {
-                Index = (1000 - (1000 % Items.Count) + Index + 1) % Items.Count;
-                Common.PlaySound("NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET");
-            }
-
-            if (Items.Count > 0) Items[Index].ProcessControls();
         }
 
         /// <summary>
@@ -136,8 +139,8 @@ namespace RAGENativeUI.PauseMenu
 
             Items[Index].Visible = true;
             Items[Index].FadeInWhenFocused = true;
-            if (Items[Index].CanBeFocused)
-                Items[Index].Focused = true;
+            //if (Items[Index].CanBeFocused)
+            //    Items[Index].Focused = true;
             Items[Index].UseDynamicPositionment = false;
             Items[Index].SafeSize = SafeSize.AddPoints(new Point((int)activeWidth - submenuWidth, 0));
             Items[Index].TopLeft = SafeSize.AddPoints(new Point((int)activeWidth - submenuWidth, 0));
