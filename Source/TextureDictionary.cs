4ec23e83e4304e0d5d5a89733e5a37ea197384cb
alexguirre <alexguirre.aa@gmail.com>
2022-07-09 20:59:16 +0200

    Add TextureDictionary struct

---
+++ Source/TextureDictionary.cs
@@ -0,0 +1,27 @@
+﻿namespace RAGENativeUI
+{
+    using Rage;
+
+    public readonly struct TextureDictionary : INamedAsset
+    {
+        // TODO: should TextureDictionary check for "embed:"?
+
+        public string Name { get; }
+
+        public bool IsLoaded => N.HasStreamedTextureDictLoaded(Name);
+
+        public TextureDictionary(string name) => Name = name;
+
+        public void Dismiss() => N.SetStreamedTextureDictAsNoLongerNeeded(Name);
+        public void Load() => N.RequestStreamedTextureDict(Name);
+        public void LoadAndWait()
+        {
+            Load();
+            var txd = this;
+            GameFiber.SleepUntil(() => txd.IsLoaded, timeOut: 10_000);
+        }
+
+        public static implicit operator TextureDictionary(string textureDictionaryName) => new(textureDictionaryName);
+        public static implicit operator string(TextureDictionary textureDictionary) => textureDictionary.Name;
+    }
+}
